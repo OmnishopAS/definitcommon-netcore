b0e863f4b9a2a17e3198788630513b738cdbe752
Anders Ulveseth <anders@definit.no>
2020-01-20 00:13:39 +0100

    Added DbExecuteUtils.cs

---
+++ src/Definit.Common.DAL/DbExecuteUtils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Definit.Common.DAL
+{
+    public static class DbExecuteUtils
+    {
+        public static T ExecuteScalar<T>(string connectionString, string sql, T defaultValue = default(T))
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                return ExecuteScalar(connection, sql, defaultValue);
+            }
+        }
+
+        public static T ExecuteScalar<T>(DbConnection connection, string sql, T defaultValue = default(T))
+        {
+            connection.Open();
+            try
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = sql;
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (!reader.HasRows)
+                            return defaultValue;
+
+                        reader.Read();
+                        if (reader.IsDBNull(0))
+                            return defaultValue;
+
+                        return (T)reader.GetValue(0);
+                    }
+                }
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
