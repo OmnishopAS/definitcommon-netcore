c8a55461cbcbb05faf187b72e53d40d9c8d0eb40
Anders Ulveseth <anders@definit.no>
2020-09-24 12:14:59 +0200

    Upgrade to .net core 3.1

--- src/Definit.Common.DAL/DbExecuteUtils.cs
+++ src/Definit.Common.DAL/DbExecuteUtils.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-using System.Data.SqlClient;
 using System.Text;
+using Microsoft.Data.SqlClient;
 
 namespace Definit.Common.DAL
 {
