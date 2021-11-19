2601214b7c6f41033d97a8f63e63eb118200ad82
Anders Ulveseth <anders@definit.no>
2021-11-19 18:57:38 +0100

    BasicAuthenticationActionFilterAttribute only parses Authorization header that starts with "Basic "

--- src/Definit.Common.Server/BasicAuthenticationActionFilterAttribute.cs
+++ src/Definit.Common.Server/BasicAuthenticationActionFilterAttribute.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Definit.Common.Server
@@ -11,11 +14,11 @@ namespace Definit.Common.Server
 
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            var req = filterContext.HttpContext.Request;
-            var auth = req.Headers["Authorization"].FirstOrDefault();
-            if (!String.IsNullOrEmpty(auth))
+            var request = filterContext.HttpContext.Request;
+            var authHeader = request.Headers["Authorization"].FirstOrDefault(x=>x.StartsWith("Basic"));
+            if (!String.IsNullOrEmpty(authHeader))
             {
-                var cred = System.Text.ASCIIEncoding.ASCII.GetString(Convert.FromBase64String(auth.Substring(6))).Split(':');
+                var cred = System.Text.ASCIIEncoding.ASCII.GetString(Convert.FromBase64String(authHeader.Substring(6))).Split(':');
                 var user = new { Name = cred[0], Pass = cred[1] };
 
                 if(ValidateCredentials(filterContext, user.Name, user.Pass))
@@ -23,6 +26,7 @@ namespace Definit.Common.Server
                     return;
                 }
             }
+            filterContext.Result = new UnauthorizedObjectResult("Access denied. User is not authenticated.");
             var res = filterContext.HttpContext.Response;
             res.StatusCode = 401;
             res.Headers.Add("WWW-Authenticate", String.Format("Basic realm=\"{0}\"", BasicRealm ?? "BasicRealm"));
